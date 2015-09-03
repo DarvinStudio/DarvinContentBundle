dd6167a86fd2ef7307b0e3883888390247a61bc1
igor <igor.sv.n@gmail.com>
2015-09-03 11:44:31 +0300

    Implement rebuild slug map command.

---
+++ Command/RebuildSlugMapCommand.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\ContentBundle\Command;
+
+use Darvin\ContentBundle\Entity\SlugMapItem;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Rebuild slug map command
+ */
+class RebuildSlugMapCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('darvin:content:rebuild-slug-map')
+            ->setDescription('Rebuilds slug map.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->truncateSlugMap();
+
+        $em = $this->getEntityManager();
+        $darvinMetadataFactory = $this->getDarvinMetadataFactory();
+
+        /** @var \Doctrine\ORM\Mapping\ClassMetadataInfo $doctrineMeta */
+        foreach ($em->getMetadataFactory()->getAllMetadata() as $doctrineMeta) {
+            if ($doctrineMeta->getReflectionClass()->isAbstract()) {
+                continue;
+            }
+
+            $darvinMeta = $darvinMetadataFactory->getMetadata($doctrineMeta);
+
+            if (!isset($darvinMeta['slugs']) || empty($darvinMeta['slugs'])) {
+                continue;
+            }
+
+            $entities = $em->getRepository($doctrineMeta->getName())->findAll();
+
+            foreach ($entities as $entity) {
+                $em->getEventManager()->dispatchEvent(Events::postPersist, new LifecycleEventArgs($entity, $em));
+
+                $output->writeln($doctrineMeta->getName().' '.implode('', $doctrineMeta->getIdentifierValues($entity)));
+            }
+        }
+    }
+
+    private function truncateSlugMap()
+    {
+        $em = $this->getEntityManager();
+
+        $tableName = $em->getClassMetadata(SlugMapItem::CLASS_NAME)->getTableName();
+
+        $connection = $em->getConnection();
+        $connection->executeQuery($connection->getDriver()->getDatabasePlatform()->getTruncateTableSQL($tableName));
+    }
+
+    /**
+     * @return \Darvin\Utils\Mapping\MetadataFactoryInterface
+     */
+    private function getDarvinMetadataFactory()
+    {
+        return $this->getContainer()->get('darvin_utils.mapping.metadata_factory');
+    }
+
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    private function getEntityManager()
+    {
+        return $this->getContainer()->get('doctrine.orm.entity_manager');
+    }
+}
