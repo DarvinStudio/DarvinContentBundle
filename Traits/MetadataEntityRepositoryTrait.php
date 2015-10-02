0ed379bb3a7daeeb53c5cf6e6682da9eaa56b1a7
igor <igor.sv.n@gmail.com>
2015-10-02 11:23:14 +0300

    Implement page menu.

---
+++ Traits/MetadataEntityRepositoryTrait.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\ContentBundle\Traits;
+
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Metadata entity repository trait
+ */
+trait MetadataEntityRepositoryTrait
+{
+    /**
+     * @param \Doctrine\ORM\QueryBuilder $qb    Query builder
+     * @param string                     $alias Alias
+     *
+     * @return MetadataEntityRepositoryTrait
+     */
+    protected function addEnabledFilter(QueryBuilder $qb, $alias = 'o')
+    {
+        $qb->andWhere($alias.'.enabled = :enabled')->setParameter('enabled', true);
+
+        return $this;
+    }
+
+    /**
+     * @param \Doctrine\ORM\QueryBuilder $qb    Query builder
+     * @param string                     $alias Alias
+     *
+     * @return MetadataEntityRepositoryTrait
+     */
+    protected function addNotHiddenFilter(QueryBuilder $qb, $alias = 'o')
+    {
+        $qb->andWhere($alias.'.hidden = :hidden')->setParameter('hidden', false);
+
+        return $this;
+    }
+}
