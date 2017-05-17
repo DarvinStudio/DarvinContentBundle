1707c5f1d4f5286a2c84b42c2127b411be87ec34
Igor Nikolaev <igor.sv.n@gmail.com>
2017-05-17 08:38:23 +0300

    Validate pagination page number.

---
+++ Pagination/PageNotFoundException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2017, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\ContentBundle\Pagination;
+
+use Darvin\ContentBundle\Exception\DarvinContentException;
+
+/**
+ * Page not found exception
+ */
+class PageNotFoundException extends DarvinContentException
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($message = 'Page not found.', $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
