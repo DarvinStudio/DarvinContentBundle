728c2fc63834adabc064210e2a1f41b9562d694b
Igor Nikolaev <igor.sv.n@gmail.com>
2019-02-13 14:20:46 +0300

    Add "nav_links()" macro.

--- EventListener/Pagination/PagerSubscriber.php
+++ EventListener/Pagination/PagerSubscriber.php
@@ -89,14 +89,14 @@ class PagerSubscriber implements EventSubscriberInterface
 
         $page = $pagination->getPage();
 
-        if (null !== $page) {
+        if (null === $page) {
+            $page = 1;
+        } else {
             $pageNumber = (int)$page;
 
             if ((string)$pageNumber !== (string)$page || $pageNumber < 0 || $pageNumber > $pagination->getPageCount()) {
                 throw new PageNotFoundException();
             }
-        } else {
-            $page = 1;
         }
 
         $request = $this->requestStack->getCurrentRequest();
