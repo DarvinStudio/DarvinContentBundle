b989fb1d151dc78418933c8b55e7d817acfa8ecc
Igor Nikolaev <igor.sv.n@gmail.com>
2019-08-15 16:17:03 +0300

    Implement dummy reposition controller.

---
+++ Controller/Sorting/RepositionController.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\ContentBundle\Controller\Sorting;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Reposition controller
+ */
+class RepositionController
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(): Response
+    {
+        return new Response();
+    }
+}
