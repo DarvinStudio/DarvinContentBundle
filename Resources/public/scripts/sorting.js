7bd7701a2aa1dae2c1dabf7aec89b12b506b5b37
Igor Nikolaev <igor.sv.n@gmail.com>
2019-08-16 09:10:24 +0300

    Implement basic sorting script.

--- Resources/public/scripts/sorting.js
+++ Resources/public/scripts/sorting.js
@@ -1,8 +1,26 @@
 (() => {
+    const SELECTOR = {
+        container: '.js-content-sortable',
+        item:      '[data-id]'
+    };
+
     let init;
     (init = function (context) {
-        $(context || 'body').find('.js-content-sortable').sortable({
-            items: '[data-id]'
+        $(context || 'body').find(SELECTOR.container).sortable({
+            items:  SELECTOR.item,
+            update: (e, ui) => {
+                let $items = ui.item.closest(SELECTOR.container).find(SELECTOR.item),
+                    ids    = [];
+
+                if ($items.length > 0) {
+                    ids = $items.map((i, item) => {
+                        return $(item).data('id');
+                    }).get();
+                }
+            }
         });
     })();
+    $(document).on('app.ajax.html', (e, args) => {
+        init(args.$html);
+    });
 })();
