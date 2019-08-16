a451fd40ae0369557ca7a45843fa7ae4ee29853e
Igor Nikolaev <igor.sv.n@gmail.com>
2019-08-16 08:54:54 +0300

    Implement basic sorting script.

--- Resources/public/scripts/sorting.js
+++ Resources/public/scripts/sorting.js
@@ -1,6 +1,8 @@
 (() => {
     let init;
     (init = function (context) {
-        $(context || 'body').find('.js-content-sortable').sortable();
+        $(context || 'body').find('.js-content-sortable').sortable({
+            items: '[data-id]'
+        });
     })();
 })();
