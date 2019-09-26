9fc77c15744bbe534a73ac77c3536f11d4044d0e
Igor Nikolaev <igor.sv.n@gmail.com>
2019-09-26 14:56:18 +0300

    Refactor switch locale event subscriber.

--- EventListener/SwitchLocaleSubscriber.php
+++ EventListener/SwitchLocaleSubscriber.php
@@ -1,34 +1,34 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2017, Darvin Studio
+ * @copyright Copyright (c) 2017-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace AppBundle\EventListener;
+namespace Darvin\ContentBundle\EventListener;
 
+use Darvin\Utils\Homepage\HomepageRouterInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Switch locale event subscriber
  */
 class SwitchLocaleSubscriber implements EventSubscriberInterface
 {
-    const SESSION_KEY = 'app_switch_locale';
+    public const SESSION_KEY = 'darvin.content.locale.switch';
 
     /**
-     * @var \Symfony\Component\Routing\RouterInterface
+     * @var \Darvin\Utils\Homepage\HomepageRouterInterface
      */
-    private $router;
+    private $homepageRouter;
 
     /**
      * @var bool
@@ -36,19 +36,19 @@ class SwitchLocaleSubscriber implements EventSubscriberInterface
     private $localeIsSwitching;
 
     /**
-     * @param \Symfony\Component\Routing\RouterInterface $router Router
+     * @param \Darvin\Utils\Homepage\HomepageRouterInterface $homepageRouter Homepage router
      */
-    public function __construct(RouterInterface $router)
+    public function __construct(HomepageRouterInterface $homepageRouter)
     {
-        $this->router = $router;
+        $this->homepageRouter = $homepageRouter;
 
         $this->localeIsSwitching = false;
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             KernelEvents::REQUEST   => 'detectLocaleSwitching',
@@ -57,9 +57,9 @@ class SwitchLocaleSubscriber implements EventSubscriberInterface
     }
 
     /**
-     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event Event
+     * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event Event
      */
-    public function detectLocaleSwitching(GetResponseEvent $event)
+    public function detectLocaleSwitching(RequestEvent $event): void
     {
         if (!$event->isMasterRequest() || !$event->getRequest()->getSession()->has(self::SESSION_KEY)) {
             return;
@@ -71,9 +71,9 @@ class SwitchLocaleSubscriber implements EventSubscriberInterface
     }
 
     /**
-     * @param \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent $event Event
+     * @param \Symfony\Component\HttpKernel\Event\ExceptionEvent $event Event
      */
-    public function redirectToHomepage(GetResponseForExceptionEvent $event)
+    public function redirectToHomepage(ExceptionEvent $event): void
     {
         if (!$event->isMasterRequest() || !$this->localeIsSwitching) {
             return;
@@ -81,8 +81,14 @@ class SwitchLocaleSubscriber implements EventSubscriberInterface
 
         $exception = $event->getException();
 
-        if ($exception instanceof HttpExceptionInterface) {
-            $event->setResponse(new RedirectResponse($this->router->generate('darvin_page_homepage')));
+        if (!$exception instanceof HttpExceptionInterface) {
+            return;
+        }
+
+        $url = $this->homepageRouter->generate();
+
+        if (null !== $url) {
+            $event->setResponse(new RedirectResponse($url));
         }
     }
 }
