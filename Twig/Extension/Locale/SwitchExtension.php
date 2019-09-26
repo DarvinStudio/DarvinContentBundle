9365b2da22e116a3a2e991a1207f73534a711fe4
Igor Nikolaev <igor.sv.n@gmail.com>
2019-09-26 15:48:06 +0300

    Implement dummy "content_locale_switcher()" Twig function.

---
+++ Twig/Extension/Locale/SwitchExtension.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\ContentBundle\Twig\Extension\Locale;
+
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * Switch locale Twig extension
+ */
+class SwitchExtension extends AbstractExtension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('content_locale_switcher', [$this, 'renderSwitcher'], [
+                'needs_environment' => true,
+                'is_safe'           => ['html'],
+            ]),
+        ];
+    }
+
+    /**
+     * @param \Twig\Environment $twig     Twig
+     * @param string            $template Template
+     * @param array             $options  Options
+     *
+     * @return string
+     */
+    public function renderSwitcher(Environment $twig, string $template = '@DarvinContent/locale_switcher.html.twig', array $options = []): string
+    {
+        return $twig->render($template, array_merge([], $options));
+    }
+}
