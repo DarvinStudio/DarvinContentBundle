d8ec54da4f1e4f9cba7510c1332f2b714fed6adf
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-06 09:09:00 +0300

    Implement dummy property embedder.

---
+++ Property/Embedder/PropertyEmbedderInterface.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\ContentBundle\Property\Embedder;
+
+/**
+ * Property embedder
+ */
+interface PropertyEmbedderInterface
+{
+    /**
+     * @param string|null $content Content
+     * @param object|null $object  Object
+     *
+     * @return string|null
+     */
+    public function embedProperties(?string $content, ?object $object = null): ?string;
+}
