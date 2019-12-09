2aca34e6ec8440f4ef3cba82af8bd8056813313e
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-09 10:46:42 +0300

    Test property embedder.

---
+++ Tests/Property/Embedder/PropertyEmbedderTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\ContentBundle\Tests\Property\Embedder;
+
+use Darvin\ContentBundle\Property\Embedder\PropertyEmbedder;
+use Darvin\Utils\Strings\Stringifier\StringifierInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+
+/**
+ * Property embedder test
+ *
+ * @group property
+ */
+class PropertyEmbedderTest extends TestCase
+{
+    /**
+     * @var \Darvin\ContentBundle\Property\Embedder\PropertyEmbedderInterface
+     */
+    private $embedder;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp(): void
+    {
+        $propertyAccessor = $this->getMockBuilder(PropertyAccessorInterface::class)->getMock();
+        $propertyAccessor->method('getValue')->willReturn('*VALUE*');
+
+        $stringifier = $this->getMockBuilder(StringifierInterface::class)->getMock();
+        $stringifier->method('stringify')->willReturnArgument(0);
+
+        $this->embedder = new PropertyEmbedder($propertyAccessor, $stringifier);
+    }
+
+    /**
+     * @dataProvider embedPropertiesProvider
+     *
+     * @param mixed $expected Expected result
+     * @param mixed $content  Content
+     * @param mixed $object   Object
+     */
+    public function testEmbedProperties($expected, $content, $object = null): void
+    {
+        self::assertEquals($expected, $this->embedder->embedProperties($content, $object));
+    }
+
+    /**
+     * @return iterable
+     */
+    public function embedPropertiesProvider(): iterable
+    {
+        yield ['', null];
+    }
+}
