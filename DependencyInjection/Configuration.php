16612a7b2bec88c71dcf91963ab263d2b17eadc4
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-27 16:06:21 +0300

    Allow to secure autocomplete functionality.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -75,7 +75,7 @@ class Configuration implements ConfigurationInterface
                                     if (1 === count($provider)) {
                                         $key = key($provider);
 
-                                        if (!in_array($key, ['service', 'method', 'options'])) {
+                                        if (!in_array($key, ['service', 'method', 'options', 'required_permissions'])) {
                                             return [
                                                 'service' => $key,
                                                 'method'  => $provider[$key],
