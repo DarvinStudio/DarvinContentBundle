117dde53a01a7a739c75f7b6848a6152e4f63963
Igor Nikolaev <igor.sv.n@gmail.com>
2020-12-09 14:37:54 +0300

    Update paginator.

--- Pagination/Paginator.php
+++ Pagination/Paginator.php
@@ -13,22 +13,68 @@ namespace Darvin\ContentBundle\Pagination;
 use Knp\Component\Pager\Event;
 use Knp\Component\Pager\Exception\PageNumberOutOfRangeException;
 use Knp\Component\Pager\Pagination\PaginationInterface;
-use Knp\Component\Pager\Paginator as BasePaginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Paginator
  */
-class Paginator extends BasePaginator
+class Paginator implements PaginatorInterface
 {
+    /**
+     * @var \Symfony\Contracts\EventDispatcher\EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    /**
+     * @var \Symfony\Component\HttpFoundation\RequestStack|null
+     */
+    private $requestStack;
+
+    /**
+     * @var array
+     */
+    private $defaultOptions;
+
+    /**
+     * @param \Symfony\Contracts\EventDispatcher\EventDispatcherInterface $eventDispatcher Event dispatcher
+     * @param \Symfony\Component\HttpFoundation\RequestStack|null         $requestStack    Request stack
+     */
+    public function __construct(EventDispatcherInterface $eventDispatcher, ?RequestStack $requestStack = null)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->requestStack = $requestStack;
+
+        $this->defaultOptions = [
+            self::PAGE_PARAMETER_NAME           => 'page',
+            self::SORT_FIELD_PARAMETER_NAME     => 'sort',
+            self::SORT_DIRECTION_PARAMETER_NAME => 'direction',
+            self::FILTER_FIELD_PARAMETER_NAME   => 'filterParam',
+            self::FILTER_VALUE_PARAMETER_NAME   => 'filterValue',
+            self::DISTINCT                      => true,
+            self::PAGE_OUT_OF_RANGE             => self::PAGE_OUT_OF_RANGE_IGNORE,
+            self::DEFAULT_LIMIT                 => self::DEFAULT_LIMIT_VALUE,
+        ];
+    }
+
+    /**
+     * @param array $options Options
+     */
+    public function setDefaultPaginatorOptions(array $options): void
+    {
+        $this->defaultOptions = array_merge($this->defaultOptions, $options);
+    }
+
     /**
      * {@inheritDoc}
      */
     public function paginate($target, int $page = 1, ?int $limit = null, array $options = []): PaginationInterface
     {
         $limit = $limit ?? $this->defaultOptions[self::DEFAULT_LIMIT];
-        if ($limit <= 0 or $page < 0) {
+        if ($limit <= 0 || $page < 0) {
             throw new \LogicException("Invalid item per page number. Limit: $limit and Page: $page, must be positive integers");
         }
 
@@ -53,14 +99,14 @@ class Paginator extends BasePaginator
 
         // before pagination start
         $beforeEvent = new Event\BeforeEvent($this->eventDispatcher, $request);
-        $this->dispatch('knp_pager.before', $beforeEvent);
+        $this->eventDispatcher->dispatch($beforeEvent, 'knp_pager.before');
         // items
         $itemsEvent = new Event\ItemsEvent($offset, 0 === $page ? PHP_INT_MAX : $limit);
         $itemsEvent->options = &$options;
         $itemsEvent->target = &$target;
 
         try {
-            $this->dispatch('knp_pager.items', $itemsEvent);
+            $this->eventDispatcher->dispatch($itemsEvent, 'knp_pager.items');
         } catch (\UnexpectedValueException $ex) {
             throw new NotFoundHttpException($ex->getMessage(), $ex);
         }
@@ -69,12 +115,15 @@ class Paginator extends BasePaginator
         }
         if ($page > ceil($itemsEvent->count / $limit)) {
             $pageOutOfRangeOption = $options[self::PAGE_OUT_OF_RANGE] ?? $this->defaultOptions[self::PAGE_OUT_OF_RANGE];
-            if ($pageOutOfRangeOption === self::PAGE_OUT_OF_RANGE_FIX) {
+            if ($pageOutOfRangeOption === self::PAGE_OUT_OF_RANGE_FIX && $itemsEvent->count > 0) {
                 // replace page number out of range with max page
-                return $this->paginate($target, ceil($itemsEvent->count / $limit), $limit, $options);
+                return $this->paginate($target, (int)ceil($itemsEvent->count / $limit), $limit, $options);
             }
-            if ($pageOutOfRangeOption === self::PAGE_OUT_OF_RANGE_THROW_EXCEPTION) {
-                throw new PageNumberOutOfRangeException("Page number: $page is out of range.");
+            if ($pageOutOfRangeOption === self::PAGE_OUT_OF_RANGE_THROW_EXCEPTION && $page > 1) {
+                throw new PageNumberOutOfRangeException(
+                    sprintf('Page number: %d is out of range.', $page),
+                    (int)ceil($itemsEvent->count / $limit)
+                );
             }
         }
 
@@ -82,7 +131,7 @@ class Paginator extends BasePaginator
         $paginationEvent = new Event\PaginationEvent;
         $paginationEvent->target = &$target;
         $paginationEvent->options = &$options;
-        $this->dispatch('knp_pager.pagination', $paginationEvent);
+        $this->eventDispatcher->dispatch($paginationEvent, 'knp_pager.pagination');
         if (!$paginationEvent->isPropagationStopped()) {
             throw new \RuntimeException('One of listeners must create pagination view');
         }
@@ -97,7 +146,8 @@ class Paginator extends BasePaginator
 
         // after
         $afterEvent = new Event\AfterEvent($paginationView);
-        $this->dispatch('knp_pager.after', $afterEvent);
+        $this->eventDispatcher->dispatch($afterEvent, 'knp_pager.after');
+
         return $paginationView;
     }
 }
